6a416ceafae2938d1dc61967c771f9bda80d47ae
Evgeniy Tatarkin <tatarkin.evg@gmail.com>
2026-03-02 10:56:39 +0300

    feat: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A CLI utility for COSMIC Wayland toplevel and workspace management ";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    naersk = {
+      url = "github:nix-community/naersk/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, naersk }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in {
+        defaultPackage = naersk-lib.buildPackage ./.;
+        devShell = with pkgs;
+          mkShell {
+            buildInputs =
+              [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
+      });
+}
