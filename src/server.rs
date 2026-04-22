aa6393bd1b12fbaeebc1c5487d05570bd62d7d8c
Evgeniy Tatarkin <tatarkin.evg@gmail.com>
2026-04-22 17:42:03 +0300

    fix: introduce server handler

--- src/server.rs
+++ src/server.rs
@@ -2,6 +2,7 @@ use futures::FutureExt;
 use jsonrpc_stdio_server::jsonrpc_core::{Error as JsonRpcError, IoHandler, Value};
 use serde::Serialize;
 use std::error::Error;
+use std::sync::Arc;
 use tokio::sync::mpsc::{UnboundedSender, unbounded_channel};
 use tokio::sync::oneshot;
 
@@ -81,11 +82,40 @@ impl Backend {
     }
 }
 
+struct ServerHandler {
+    tx: UnboundedSender<BackendRequest>,
+}
+
+impl ServerHandler {
+    fn new(tx: UnboundedSender<BackendRequest>) -> Self {
+        Self { tx }
+    }
+    async fn handle_request(
+        self: Arc<Self>,
+        request_params: BackendRequestParams,
+    ) -> Result<serde_json::Value, JsonRpcError> {
+        let (response_tx, request) = BackendRequest::request(request_params);
+        self.tx.send(request).map_err(|e| JsonRpcError {
+            message: e.to_string(),
+            ..JsonRpcError::internal_error()
+        })?;
+        let response = response_tx.await.map_err(|e| JsonRpcError {
+            message: e.to_string(),
+            ..JsonRpcError::internal_error()
+        })?;
+        serde_json::to_value(response).map_err(|e| JsonRpcError {
+            message: e.to_string(),
+            ..JsonRpcError::internal_error()
+        })
+    }
+}
+
 pub async fn run(mut backend: Backend) -> Result<(), Box<dyn Error>> {
     let mut io = IoHandler::new();
 
     let mut watch_rx = backend.app_state.enable_notify();
     let (_handle, request_tx) = backend.start()?;
+    let server_handler = Arc::new(ServerHandler::new(request_tx));
 
     let _handle = tokio::spawn(async move {
         while let Ok(_) = watch_rx.changed().await {
@@ -101,23 +131,10 @@ pub async fn run(mut backend: Backend) -> Result<(), Box<dyn Error>> {
     });
 
     io.add_method("info", move |_params| {
-        let request_tx_clone = request_tx.clone();
-        async move {
-            let (response_tx, request) = BackendRequest::request(BackendRequestParams::GetInfo);
-            request_tx_clone.send(request).map_err(|e| JsonRpcError {
-                message: e.to_string(),
-                ..JsonRpcError::internal_error()
-            })?;
-            let response = response_tx.await.map_err(|e| JsonRpcError {
-                message: e.to_string(),
-                ..JsonRpcError::internal_error()
-            })?;
-            serde_json::to_value(response).map_err(|e| JsonRpcError {
-                message: e.to_string(),
-                ..JsonRpcError::internal_error()
-            })
-        }
-        .boxed()
+        server_handler
+            .clone()
+            .handle_request(BackendRequestParams::GetInfo)
+            .boxed()
     });
 
     io.add_method("move", |_params| {
